b9b0088c264b26d8563f93baa17d0c9957a69e1c
Bhagvan Kommadi <bhagvan.kommadi@apples-MacBook-Air.local>
2019-01-15 18:46:08 +0530

    added chapter 2

---
+++ ch2/Bell/Bell.qs
@@ -0,0 +1,38 @@
+namespace Bell
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+
+    operation Set (desired: Result, q1: Qubit) : Unit
+    {
+        let current = M(q1);
+        if (desired != current)
+        {
+            X(q1);
+        }
+    }
+
+    operation BellTest (count : Int, initial: Result) : (Int, Int)
+    {
+        mutable numOnes = 0;
+        using (qubit = Qubit())
+        {
+            for (test in 1..count)
+            {
+                Set (initial, qubit);
+
+                let res = M (qubit);
+
+                if (res == One)
+                {
+                    set numOnes = numOnes + 1;
+                }
+            }
+            Set(Zero, qubit);
+        }
+
+        return (count-numOnes, numOnes);
+    }
+
+    
+}
---
+++ ch2/Bell/obj/qsharp/src/Bell.g.cs
@@ -0,0 +1,181 @@
+#pragma warning disable 1591
+using System;
+using Microsoft.Quantum.Core;
+using Microsoft.Quantum.Primitive;
+using Microsoft.Quantum.Simulation.Core;
+
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.CallableDeclaration("{\"Kind\":{\"Case\":\"Operation\"},\"QualifiedName\":{\"Namespace\":\"Bell\",\"Name\":\"Set\"},\"SourceFile\":\"/Users/bhagvan.kommadi/Desktop/packt/Q%23/Bell/Bell.qs\",\"Position\":{\"Item1\":5,\"Item2\":4},\"SymbolRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":14}},\"ArgumentTuple\":{\"Case\":\"QsTuple\",\"Fields\":[[{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"desired\"]},\"Type\":{\"Case\":\"Result\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":16},\"Item2\":{\"Line\":1,\"Column\":23}}}]},{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"q1\"]},\"Type\":{\"Case\":\"Qubit\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":33},\"Item2\":{\"Line\":1,\"Column\":35}}}]}]]},\"Signature\":{\"TypeParameters\":[],\"ArgumentType\":{\"Case\":\"TupleType\",\"Fields\":[[{\"Case\":\"Result\"},{\"Case\":\"Qubit\"}]]},\"ReturnType\":{\"Case\":\"UnitType\"},\"SupportedFunctors\":[]},\"Documentation\":[]}")]
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.SpecializationDeclaration("{\"Kind\":{\"Case\":\"QsBody\"},\"Parent\":{\"Namespace\":\"Bell\",\"Name\":\"Set\"},\"SourceFile\":\"/Users/bhagvan.kommadi/Desktop/packt/Q%23/Bell/Bell.qs\",\"Position\":{\"Item1\":5,\"Item2\":4},\"HeaderRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":14}},\"Documentation\":[]}")]
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.CallableDeclaration("{\"Kind\":{\"Case\":\"Operation\"},\"QualifiedName\":{\"Namespace\":\"Bell\",\"Name\":\"BellTest\"},\"SourceFile\":\"/Users/bhagvan.kommadi/Desktop/packt/Q%23/Bell/Bell.qs\",\"Position\":{\"Item1\":14,\"Item2\":4},\"SymbolRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":19}},\"ArgumentTuple\":{\"Case\":\"QsTuple\",\"Fields\":[[{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"count\"]},\"Type\":{\"Case\":\"Int\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":21},\"Item2\":{\"Line\":1,\"Column\":26}}}]},{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"initial\"]},\"Type\":{\"Case\":\"Result\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":34},\"Item2\":{\"Line\":1,\"Column\":41}}}]}]]},\"Signature\":{\"TypeParameters\":[],\"ArgumentType\":{\"Case\":\"TupleType\",\"Fields\":[[{\"Case\":\"Int\"},{\"Case\":\"Result\"}]]},\"ReturnType\":{\"Case\":\"TupleType\",\"Fields\":[[{\"Case\":\"Int\"},{\"Case\":\"Int\"}]]},\"SupportedFunctors\":[]},\"Documentation\":[]}")]
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.SpecializationDeclaration("{\"Kind\":{\"Case\":\"QsBody\"},\"Parent\":{\"Namespace\":\"Bell\",\"Name\":\"BellTest\"},\"SourceFile\":\"/Users/bhagvan.kommadi/Desktop/packt/Q%23/Bell/Bell.qs\",\"Position\":{\"Item1\":14,\"Item2\":4},\"HeaderRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":19}},\"Documentation\":[]}")]
+#line hidden
+namespace Bell
+{
+    public class Set : Operation<(Result,Qubit), QVoid>, ICallable
+    {
+        public Set(IOperationFactory m) : base(m)
+        {
+        }
+
+        public class In : QTuple<(Result,Qubit)>, IApplyData
+        {
+            public In((Result,Qubit) data) : base(data)
+            {
+            }
+
+            System.Collections.Generic.IEnumerable<Qubit> IApplyData.Qubits
+            {
+                get
+                {
+                    yield return Data.Item2;
+                }
+            }
+        }
+
+        String ICallable.Name => "Set";
+        String ICallable.FullName => "Bell.Set";
+        protected ICallable<Qubit, Result> M
+        {
+            get;
+            set;
+        }
+
+        protected IUnitary<Qubit> MicrosoftQuantumPrimitiveX
+        {
+            get;
+            set;
+        }
+
+        public override Func<(Result,Qubit), QVoid> Body => (__in) =>
+        {
+            var (desired,q1) = __in;
+#line 8 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+            var current = M.Apply(q1);
+#line 9 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+            if ((desired != current))
+            {
+#line 11 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                MicrosoftQuantumPrimitiveX.Apply(q1);
+            }
+
+#line hidden
+            return QVoid.Instance;
+        }
+
+        ;
+        public override void Init()
+        {
+            this.M = this.Factory.Get<ICallable<Qubit, Result>>(typeof(Microsoft.Quantum.Primitive.M));
+            this.MicrosoftQuantumPrimitiveX = this.Factory.Get<IUnitary<Qubit>>(typeof(Microsoft.Quantum.Primitive.X));
+        }
+
+        public override IApplyData __dataIn((Result,Qubit) data) => new In(data);
+        public override IApplyData __dataOut(QVoid data) => data;
+        public static System.Threading.Tasks.Task<QVoid> Run(IOperationFactory __m__, Result desired, Qubit q1)
+        {
+            return __m__.Run<Set, (Result,Qubit), QVoid>((desired, q1));
+        }
+    }
+
+    public class BellTest : Operation<(Int64,Result), (Int64,Int64)>, ICallable
+    {
+        public BellTest(IOperationFactory m) : base(m)
+        {
+        }
+
+        public class In : QTuple<(Int64,Result)>, IApplyData
+        {
+            public In((Int64,Result) data) : base(data)
+            {
+            }
+
+            System.Collections.Generic.IEnumerable<Qubit> IApplyData.Qubits => null;
+        }
+
+        public class Out : QTuple<(Int64,Int64)>, IApplyData
+        {
+            public Out((Int64,Int64) data) : base(data)
+            {
+            }
+
+            System.Collections.Generic.IEnumerable<Qubit> IApplyData.Qubits => null;
+        }
+
+        String ICallable.Name => "BellTest";
+        String ICallable.FullName => "Bell.BellTest";
+        protected ICallable<(Result,Qubit), QVoid> Set
+        {
+            get;
+            set;
+        }
+
+        protected Allocate Allocate
+        {
+            get;
+            set;
+        }
+
+        protected ICallable<Qubit, Result> M
+        {
+            get;
+            set;
+        }
+
+        protected Release Release
+        {
+            get;
+            set;
+        }
+
+        public override Func<(Int64,Result), (Int64,Int64)> Body => (__in) =>
+        {
+            var (count,initial) = __in;
+#line 17 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+            var numOnes = 0L;
+#line hidden
+            {
+#line 18 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                var qubit = Allocate.Apply();
+#line 20 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                foreach (var test in new Range(1L, count))
+#line hidden
+                {
+#line 22 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                    Set.Apply((initial, qubit));
+#line 24 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                    var res = M.Apply(qubit);
+#line 27 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                    if ((res == Result.One))
+                    {
+#line 29 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                        numOnes = (numOnes + 1L);
+                    }
+                }
+
+#line 32 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+                Set.Apply((Result.Zero, qubit));
+#line hidden
+                Release.Apply(qubit);
+            }
+
+#line 36 "/Users/bhagvan.kommadi/Desktop/packt/Q#/Bell/Bell.qs"
+            return ((count - numOnes), numOnes);
+        }
+
+        ;
+        public override void Init()
+        {
+            this.Set = this.Factory.Get<ICallable<(Result,Qubit), QVoid>>(typeof(Set));
+            this.Allocate = this.Factory.Get<Allocate>(typeof(Microsoft.Quantum.Primitive.Allocate));
+            this.M = this.Factory.Get<ICallable<Qubit, Result>>(typeof(Microsoft.Quantum.Primitive.M));
+            this.Release = this.Factory.Get<Release>(typeof(Microsoft.Quantum.Primitive.Release));
+        }
+
+        public override IApplyData __dataIn((Int64,Result) data) => new In(data);
+        public override IApplyData __dataOut((Int64,Int64) data) => new Out(data);
+        public static System.Threading.Tasks.Task<(Int64,Int64)> Run(IOperationFactory __m__, Int64 count, Result initial)
+        {
+            return __m__.Run<BellTest, (Int64,Result), (Int64,Int64)>((count, initial));
+        }
+    }
+}
